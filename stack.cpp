916365af2bf2e28794ca3a954df865eeb684c816
Aya <163719405+sapimouuu@users.noreply.github.com>
2026-04-22 00:43:37 +0800

    stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,2 +1,31 @@
+#include <iostream>
 #include "stack.h"
 
+void init(Stack* s) {
+    s->top = &(s->data[-1]); 
+}
+
+bool isEmpty(const Stack* s) {
+    return s->top == &(s->data[-1]);
+}
+
+bool isFull(const Stack* s) {
+    return s->top == &(s->data[MAX - 1]);
+}
+
+void push(Stack* s, int value) {
+    if (!isFull(s)) {
+        s->top++;
+        *(s->top) = value;
+    }
+}
+
+void pop(Stack* s) {
+    if (!isEmpty(s)) {
+        s->top--;
+    }
+}
+
+int peek(const Stack* s) {
+    return *(s->top);
+}
